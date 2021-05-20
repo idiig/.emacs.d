30ba7fe289da58caea12b4c4bbd96ad477ae4680
idiig <avionplat@hotmail.com>
2021-05-21 08:08:34 +0900

    modified org sub-superscripts setting

--- core/custom.el
+++ core/custom.el
@@ -7,6 +7,7 @@
  '(eshell-directory-name "~/.emacs.d/.cache/eshell/")
  '(highlight-parentheses-colors '("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900"))
  '(lsp-session-file "/Users/idiig/.emacs.d/.cache/.lsp-session-v1")
+ '(org-export-with-sub-superscripts '{})
  '(org-roam-db-location "/Users/idiig/Nutstore/org-files/org-roam.db")
  '(package-selected-packages
    '(magic-latex-buffer company-reftex company-auctex auctex-latexmk auctex tex org-roam-bibtex valign sky-color-clock org-journal org-roam pyim-basedict org-plus-contrib noflet multiple-cursors org-sticky-header git-timemachine git-messenger git-link smeargle orgit gitignore-templates gitconfig-mode flymake-diagnostic-at-point flymake-posframe appt evil-org ox-hugo org-pomodoro ox-reveal doom-modeline company-lsp lsp-ui dap-mode lsp-treemacs lsp-ivy yapfify pytest python-mode pyvenv org-ref cdlatex window-numbering which-key wgrep use-package unfill smartparens popwin mwim mozc-popup ivy-prescient iedit hungry-delete highlight-parentheses fuzzy expand-region exec-path-from-shell evil-surround evil-nerd-commenter evil-leader elpy ein diminish counsel company-statistics company-quickhelp company-fuzzy auto-yasnippet auto-complete)))
