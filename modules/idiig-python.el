6704ad85be16d23228d63a46ac301ea861107c5a
idiig <avionplat@hotmail.com>
2022-10-29 00:56:52 +0900

    config after

--- modules/idiig-python.el
+++ modules/idiig-python.el
@@ -1,6 +1,15 @@
 (use-package python
-  :defer 2 
+  :defer t 
   :init  
+  (progn
+    (add-hook 'python-hook
+              (lambda ()
+                (add-hook 'post-self-insert-hook
+                          #'python-electric-pair-string-delimiter 'append t)))
+    (idiig/register-repl 'python-mode 'python)
+    (add-hook 'python-hook 'idiig/run-prog-mode-hooks)
+    (add-hook 'python-hook 'company-mode))
+  :config
   (progn
     ;; long quotes
     (defun python-electric-pair-string-delimiter ()
@@ -10,24 +19,12 @@
                    (while (eq (char-before (- (point) count)) last-command-event)
                      (setq count (1+ count)))
                    (= count 3)))
-        (save-excursion (insert (make-string 3 last-command-event)))))
-
-    (add-hook 'python-hook
-              (lambda ()
-                (add-hook 'post-self-insert-hook
-                          #'python-electric-pair-string-delimiter 'append t)))
-    
-    (idiig/register-repl 'python-mode 'python)
-    (add-hook 'python-hook 'idiig/run-prog-mode-hooks)
-    (add-hook 'python-hook 'company-mode)
-    )
-  )
+        (save-excursion (insert (make-string 3 last-command-event)))))))
 
 (with-eval-after-load 'python
   (setq
    python-shell-interpreter "ipython3"
    python-shell-interpreter-args "-i")
-
   ;; 虚拟环境
   (use-package pyvenv
     :commands (pyvenv-workon pyvenv-activate)
